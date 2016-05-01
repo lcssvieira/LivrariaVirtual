6927f46760dc1a59302abd3dcdc2d2be48882dd5
Vieira, Lucas <lucas.vieira@venki.com.br>
2016-04-30 23:56:22 -0300

    Ajustes AdminControl

--- src/control/AdminControl.java
+++ src/control/AdminControl.java
@@ -1,28 +1,9 @@
 package control;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import dao.UsuarioDAO;
-import model.Usuario;
-
 public class AdminControl {
 	UsuarioDAO dao = null;
 
 	public AdminControl() {
 		dao = new UsuarioDAO();
-	}
-
-	public void login(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		String username = req.getParameter("user");
-		String password = req.getParameter("password");
-		// se encontra na base um usuario com login e senha, envia pra tela
-		// principal
-		if (dao.login(new Usuario(username, password)) != null)
-			req.getRequestDispatcher("inicio.jsp").forward(req, res);
-		else {
-			req.setAttribute("erro", "Usuário inválido, por favor verifique login e senha.");
-			req.getRequestDispatcher("login.jsp").forward(req, res);
-		}
 	}
 }
