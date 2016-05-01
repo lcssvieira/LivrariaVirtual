c467957b40365f02298635a284f35ed9a1ac8ba2
Vieira, Lucas <lucas.vieira@venki.com.br>
2016-04-30 23:10:17 -0300

    Ajustes CategoriaControl

--- src/control/CategoriaLivroControl.java
+++ src/control/CategoriaLivroControl.java
@@ -2,175 +2,94 @@ package control;
 
 import java.text.ParseException;
 import java.util.ArrayList;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.google.gson.Gson;
-
-import dao.ExposicaoDAO;
-import dao.EditoraDAO;
-import dao.LivroDAO;
-import model.Exposicao;
-import model.Editora;
-import model.Livro;
-import utils.MuseuUtils;
+import dao.CategoriaLivroDAO;
+import model.CategoriaLivro;
 
 public class CategoriaLivroControl {
-	ExposicaoDAO exposicaoDao;
-	LivroDAO livroDAO;
-	ExposicaoDAO dao;
-	ArrayList<Exposicao> exposicoes = new ArrayList<>();
-	ArrayList<Livro> obras = new ArrayList<>();
-	ArrayList<Editora> museus = new ArrayList<>();
-
+	CategoriaLivroDAO dao;
+	ArrayList<CategoriaLivro>categorias;
+	
 	public CategoriaLivroControl() {
-		exposicaoDao = new ExposicaoDAO();
-		dao = new ExposicaoDAO();
-		livroDAO = new LivroDAO();
+		dao = new CategoriaLivroDAO();
+		categorias = new ArrayList<>();
 	}
 
+	// cadastra um novo objeto
 	public void cadastrar(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		if (Boolean.parseBoolean(req.getParameter("alterar"))) {
 			alterar(req, res);
 			return;
 		}
-		Exposicao vo = loadParameters(req);
-		boolean cadastrado = dao.cadastrar(vo);
+		CategoriaLivro categoria = loadParameters(req);
+		boolean cadastrado = dao.inserir(categoria);
 		req.setAttribute("adicionado", cadastrado);
 		if (cadastrado)
-			listarExposicoes(req, res);
+			listarCategorias(req, res);
 		else {
-			req.setAttribute("exposicaoEditar", vo);
+			req.setAttribute("exposicaoEditar", categoria);
 			req.getRequestDispatcher("exposicao.jsp").forward(req, res);
 		}
 	}
-
-	public void listarExposicoes(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		exposicoes = dao.carregaLista();
-		req.setAttribute("listaExposicoes", exposicoes);
-		req.getRequestDispatcher("exposicaoConsulta.jsp").forward(req, res);
+	// carrega lista de objetos cadastrados , a partir do banco de dados
+	public void listarCategorias(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		categorias = dao.carregaLista();
+		req.setAttribute("listaCategorias", categorias);
+		req.getRequestDispatcher("categoriaConsulta.jsp").forward(req, res);
 	}
-
-	private Exposicao loadParameters(HttpServletRequest req) throws ParseException {
-		Exposicao exposicao = new Exposicao();
-		EditoraDAO museuDAO = new EditoraDAO();
-		LivroDAO obrasDAO = new LivroDAO();
-		Editora museu = museuDAO.selectByPk(Integer.parseInt(req.getParameter("museuSelecionado")));
-		exposicao.setMuseu(museu);
-		exposicao.setObras(obras);
-		String listaObras = req.getParameter("obrasId");
-		exposicao.setListaIds(listaObras);
-		if (listaObras.startsWith(","))
-			listaObras = listaObras.substring(1, listaObras.length());
-
-		int[] ids = new int[listaObras.split(",").length];
-		try {
-			for (int i = 0; i < ids.length; i++)
-				ids[i] = Integer.parseInt(listaObras.split(",")[i]);
-		} catch (Exception e) {
-			e.getMessage();
-		}
-		ArrayList<Livro> obras = new ArrayList<Livro>();
-		for (int id : ids) {
-			Livro o = obrasDAO.selectByPk(id);
-			if (o != null)
-				obras.add(o);
-		}
-		exposicaoDao.deletarObras(exposicao);
-		exposicao.setObras(obras);
-		exposicao.setNome(req.getParameter("nome"));
-		exposicao.setDataInicio(MuseuUtils.converteStringEmData(req.getParameter("dataInicio")));
-		exposicao.setDataFim(MuseuUtils.converteStringEmData(req.getParameter("dataFim")));
-		exposicao.setDescricao(req.getParameter("descricao"));
-		exposicao.setSecao(req.getParameter("secao"));
-		if (req.getParameter("valor_ingresso")!= null){
-			Double valor = MuseuUtils.converteMoneyTextEmDouble(req.getParameter("valor_ingresso"));
-			exposicao.setValor_ingresso(valor);
-		}
-			
-		return exposicao;
+	// cria um  objeto com dados enviados por parametro
+	private CategoriaLivro loadParameters(HttpServletRequest req) throws ParseException {
+		CategoriaLivro categoria = new CategoriaLivro();
+		categoria.setDescricao(req.getParameter("descricao"));
+		return categoria;
 	}
-	
+	// deleta um objeto
 	public void deletar(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		int id = Integer.parseInt(req.getParameter("id"));
-		if (id != 0) {
-			if (dao == null)
-				dao = new ExposicaoDAO();
+		if (id != 0) 
 			dao.deletar(id);
-		}
-		listarExposicoes(req, res);
+		
+		listarCategorias(req, res);
 	}
-
+	// carrega o CRUD com dados de um objeto
 	public void editar(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		int id = Integer.parseInt(req.getParameter("id"));
 		if (id != 0) {
-			Exposicao exposicao = dao.selectByPk(id);
-			if (exposicao != null) {
-				EditoraDAO museuDAO = new EditoraDAO();
-				museus = museuDAO.carregaLista();
-				req.setAttribute("listaMuseus", museus);
-				LivroDAO dao = new LivroDAO();
-				obras = dao.carregaLista();
-				req.setAttribute("listaObras", obras);
-				
-				req.setAttribute("exposicaoEditar", exposicao);
-				req.getRequestDispatcher("exposicao.jsp").forward(req, res);
+			CategoriaLivro categoria = dao.selectByPk(id);
+			if (categoria != null) {
+				req.setAttribute("categoriaEditar", categoria);
+				req.getRequestDispatcher("categoria.jsp").forward(req, res);
 			}
 		}
 	}
-
+	// atualiza um objeto
 	public void alterar(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		Exposicao vo = loadParameters(req);
-		vo.setId(Integer.parseInt(req.getParameter("id")));
-		boolean alterado = dao.alterar(vo);
-		String pageReturn = "exposicao.jsp";
+		CategoriaLivro categoria = loadParameters(req);
+		categoria.setId(Long.parseLong(req.getParameter("id")));
+		boolean alterado = dao.atualizar(categoria);
+		String pageReturn = "categoria.jsp";
 		if (alterado) {
-			listarExposicoes(req, res);
+			listarCategorias(req, res);
 			return;
 		}
-		req.setAttribute("exposicaoEditar", vo);
+		req.setAttribute("categoriaEditar", categoria);
 		req.getRequestDispatcher(pageReturn).forward(req, res);
 	}
 
-	public void adicionarNaList(int obraId) {
-		this.obras.add(livroDAO.selectByPk(obraId));
-	}
-
-	public void listarDados(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		LivroDAO dao = new LivroDAO();
-		obras = dao.carregaLista();
-		req.setAttribute("listaObras", obras);
-		
-		EditoraDAO museuDAO = new EditoraDAO();
-		museus = museuDAO.carregaLista();
-		req.setAttribute("listaMuseus", museus);
-	
-		req.setAttribute("exposicaoEditar", new Exposicao());
-		req.getRequestDispatcher("exposicao.jsp").forward(req, res);
-	}
-
-	public String getObraById(HttpServletRequest req, HttpServletResponse res) throws NumberFormatException, Exception {
-		String json = new Gson().toJson(new LivroControl().selectById(Integer.parseInt(req.getParameter("id"))));
-
+	// obtem o objeto em formato Json
+	public String getCategoriaJson (HttpServletRequest req, HttpServletResponse res) throws NumberFormatException, Exception {
+		CategoriaLivro categoria = dao.selectByPk(Integer.parseInt(req.getParameter("id")));
+		String json = new Gson().toJson(categoria);
 		res.setContentType("application/json");
 		res.setCharacterEncoding("UTF-8");
 		res.getWriter().print(json);
-
 		return json;
 	}
 	
-	public Exposicao selectById(int id) throws Exception {
+	// obtem o obejto a partir do seu ID
+	public CategoriaLivro selectById(int id) throws Exception {
 		return dao.selectByPk(id);
-	}
-	
-	public String getExposicaoById(HttpServletRequest req, HttpServletResponse res) throws NumberFormatException, Exception {
-		String json = new Gson().toJson(this.selectById(Integer.parseInt(req.getParameter("id"))));
-
-		res.setContentType("application/json");
-		res.setCharacterEncoding("UTF-8");
-		res.getWriter().print(json);
-
-		return json;
 	}
 }
